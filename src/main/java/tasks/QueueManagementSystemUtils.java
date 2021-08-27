5d7077f6ee9ddc73fe7c61f49033edf0bad91d1d
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-08-27 22:11:48 +0300

    QueueManagementSystemUtils calcMedianVisits(initial)

--- src/main/java/tasks/QueueManagementSystemUtils.java
+++ src/main/java/tasks/QueueManagementSystemUtils.java
@@ -36,4 +36,34 @@ public class QueueManagementSystemUtils {
         double length = systems.length;
         return calcTotalVisits(systems) / length;
     }
+    /**
+     * Calculates the median of the number of issued tickets.
+     *
+     * @cpu O(n^2)
+     * @ram O(1)
+     *
+     * @param systems QueueManagementSystem[]
+     * @return the median of the number of tickets issued from all queuing systems.
+     */
+    public static double calcMedianVisits(QueueManagementSystem[] systems) {
+        for (int n = systems.length; n > 1; n--) {
+            for(int i = 1; i < n; i++) {
+                if(systems[i -1].total > systems[i].total) {
+                    int t = systems[i - 1].total;
+                    systems[i - 1].total = systems[i].total;
+                    systems[i].total = t;
+                }
+            }
+        }
+        if (systems.length == 0) {
+            return 0;
+        }
+        if (systems.length == 1) {
+            return systems[0].total * 1.0;
+        }
+        if (systems.length % 2 == 0) {
+            return (systems[systems.length / 2 - 1].total + systems[systems.length / 2].total) / 2.0;
+        }
+        return systems[systems.length / 2].total * 1.0;
+    }
 }
--- src/test/java/tasks/QueueManagementSystemUtilsTests.java
+++ src/test/java/tasks/QueueManagementSystemUtilsTests.java
@@ -74,4 +74,54 @@ class QueueManagementSystemUtilsTests {
         QueueManagementSystem[] abc = new QueueManagementSystem[] {a, b, c};
         Assertions.assertEquals(2.3333333333333335, QueueManagementSystemUtils.calcAverageVisits(abc));
     }
+
+    @Test
+    public void calcMedianVisitsIfNil() {
+        QueueManagementSystem[] a = new QueueManagementSystem[] {};
+        Assertions.assertEquals(0, QueueManagementSystemUtils.calcMedianVisits(a));
+    }
+
+    @Test
+    public void calcMedianVisitsIfOne() {
+        QueueManagementSystem a = new QueueManagementSystem("a");
+        a.getNextTicket();
+        QueueManagementSystem[] actual = new QueueManagementSystem[] {a};
+        Assertions.assertEquals(1, QueueManagementSystemUtils.calcMedianVisits(actual));
+    }
+
+    @Test
+    public void calcMedianVisitsSevenTicketsOfThreeQueue() {
+        QueueManagementSystem a = new QueueManagementSystem("a");
+        a.getNextTicket();
+        QueueManagementSystem b = new QueueManagementSystem("b");
+        b.getNextTicket();
+        b.getNextTicket();
+        b.getNextTicket();
+        b.getNextTicket();
+        QueueManagementSystem c = new QueueManagementSystem("c");
+        c.getNextTicket();
+        c.getNextTicket();
+        QueueManagementSystem[] abc = new QueueManagementSystem[] {a, b, c};
+        Assertions.assertEquals(2, QueueManagementSystemUtils.calcMedianVisits(abc));
+    }
+
+    @Test
+    public void calcMedianVisitsSevenTicketsOfFourQueue() {
+        QueueManagementSystem a = new QueueManagementSystem("a");
+        a.getNextTicket();
+        QueueManagementSystem b = new QueueManagementSystem("b");
+        b.getNextTicket();
+        b.getNextTicket();
+        b.getNextTicket();
+        b.getNextTicket();
+        QueueManagementSystem c = new QueueManagementSystem("c");
+        c.getNextTicket();
+        c.getNextTicket();
+        c.getNextTicket();
+        QueueManagementSystem d = new QueueManagementSystem("d");
+        d.getNextTicket();
+        d.getNextTicket();
+        QueueManagementSystem[] abcd = new QueueManagementSystem[] {a, b, c, d};
+        Assertions.assertEquals(2.5, QueueManagementSystemUtils.calcMedianVisits(abcd));
+    }
 }
