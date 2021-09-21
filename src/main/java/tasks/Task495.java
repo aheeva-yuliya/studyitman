6e71f0300dacc272c32f16a7523c21812a460ed0
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-09-21 18:28:13 +0300

    Task495 solveByTwoPointers (edited)

--- src/main/java/tasks/Task495.java
+++ src/main/java/tasks/Task495.java
@@ -34,13 +34,23 @@ public class Task495 {
         return result;
     }
 
+    /**
+     * Finds the number of pairs i and j for which i < j and a[i] + a[j] = k.
+     *
+     * cpu O(n) n = a.length
+     * @ram O(1)
+     *
+     * @param a int[]
+     * @param k int argument
+     * @return a number of pairs.
+     */
     public static long solveByTwoPointers(int[] a, int k) {
         long count = 0;
         int j = a.length - 1;
         int r = j;
         for (int i = 0; i < r + 1; i++) {
             j = r;
-            while (a[i] + a[j] > k) {
+            while (a[i] + a[j] > k && i < j) {
                 j--;
             }
             if (a[i] + a[j] == k) {
--- src/test/java/tasks/Task495Tests.java
+++ src/test/java/tasks/Task495Tests.java
@@ -20,8 +20,32 @@ class Task495Tests {
     }
 
     @Test
-    public void solveByTwoPointers() {
+    public void solveByTwoPointersNormalCase() {
         int[] a = new int[] {1, 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 8, 9, 9, 10};
         Assertions.assertEquals(8, Task495.solveByTwoPointers(a, 6));
+    }
+
+    @Test
+    public void solveByTwoPointersExample() {
+        int[] a = new int[] {-1, 1, 2, 3, 5, 10};
+        Assertions.assertEquals(2, Task495.solveByTwoPointers(a, 4));
+    }
+
+    @Test
+    public void solveByTwoPointersLengthIsOne() {
+        int[] a = new int[] {2};
+        Assertions.assertEquals(0, Task495.solveByTwoPointers(a, 2));
+    }
+
+    @Test
+    public void solveByTwoPointersIfNull() {
+        int[] a = new int[] {};
+        Assertions.assertEquals(0, Task495.solveByTwoPointers(a, 2));
+    }
+
+    @Test
+    public void solveByTwoPointersIfLengthIsTwo() {
+        int[] a = new int[] {2, 2};
+        Assertions.assertEquals(1, Task495.solveByTwoPointers(a, 4));
     }
 }
