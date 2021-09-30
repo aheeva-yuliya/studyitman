2715aa44660ef6169415ca2eb71d5208afc9367f
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-09-30 16:31:35 +0300

    510 (tests edited)

--- src/test/java/tasks/Task510Tests.java
+++ src/test/java/tasks/Task510Tests.java
@@ -1,6 +1,5 @@
 package tasks;
 
-import benchmarks.SortBenchmark;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +8,6 @@ class Task510Tests {
     @Test
     public void mostFrequentIfSome() {
         int[] a = new int[] {1, 2, 5, 1, 2, 2, 3, 1};
-        Assertions.assertEquals(1, Task510.mostFrequent(a));
-    }
-
-    @Test
-    public void mostFrequentIfLengthIsOne() {
-        int[] a = new int[] {1};
         Assertions.assertEquals(1, Task510.mostFrequent(a));
     }
 
