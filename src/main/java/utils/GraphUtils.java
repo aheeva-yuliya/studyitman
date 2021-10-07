99e2d9845a0acda4948482138f204c785f4d0d7d
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-10-07 11:49:59 +0300

    GraphUtils toUndirectedAdjacencyList(initial)

--- src/main/java/utils/GraphUtils.java
+++ src/main/java/utils/GraphUtils.java
@@ -19,12 +19,36 @@ public class GraphUtils {
      * @return an adjacency list for a given graph.
      */
     public static ArrayList[] toAdjacencyList(int vertices, Pair[] edges){
+        ArrayList[] adjacency = create(vertices);
+        for (Pair edge : edges) {
+            adjacency[edge.getLeft()].add(edge.getRight());
+        }
+        return adjacency;
+    }
+
+    private static ArrayList[] create(int vertices) {
         ArrayList[] adjacency = new ArrayList[vertices + 1];
         for (int i = 0; i < adjacency.length; i++) {
             adjacency[i] = new ArrayList();
         }
+        return adjacency;
+    }
+
+    /**
+     * Returns an adjacency list for a given graph.
+     *
+     * @cpu (n + m) n = vertices and m = edges.length
+     * @ram (n + m) n = vertices and m = edges.length
+     *
+     * @param vertices int argument
+     * @param edges Pair[] represents an array of edges in an undirected graph
+     * @return an adjacency list for a given graph.
+     */
+    public static ArrayList[] toUndirectedAdjacencyList(int vertices, Pair[] edges){
+        ArrayList[] adjacency = create(vertices);
         for (Pair edge : edges) {
             adjacency[edge.getLeft()].add(edge.getRight());
+            adjacency[edge.getRight()].add(edge.getLeft());
         }
         return adjacency;
     }
--- src/test/java/utils/GraphUtilsTests.java
+++ src/test/java/utils/GraphUtilsTests.java
@@ -39,4 +39,38 @@ class GraphUtilsTests {
             Assertions.assertTrue(expected[i].equals(actual[i]));
         }
     }
+
+    @Test
+    public void toUndirectedAdjacencyListNormalCase() {
+        Pair[] edges = new Pair[]{
+                new Pair(1, 2),
+                new Pair(1, 3)
+        };
+        ArrayList[] expected = new ArrayList[]{
+                ArrayList.of(),
+                ArrayList.of(2, 3),
+                ArrayList.of(1),
+                ArrayList.of(1)
+        };
+        ArrayList[] actual = GraphUtils.toUndirectedAdjacencyList(3, edges);
+        for (int i = 0; i < expected.length; i++) {
+            Assertions.assertTrue(expected[i].equals(actual[i]));
+        }
+    }
+
+    @Test
+    public void toUndirectedAdjacencyListEdgesEmpty() {
+        Pair[] edges = new Pair[]{};
+        ArrayList[] expected = new ArrayList[]{
+                ArrayList.of(),
+                ArrayList.of(),
+                ArrayList.of(),
+                ArrayList.of()
+        };
+        ArrayList[] actual = GraphUtils.toUndirectedAdjacencyList(3, edges);
+        for (int i = 0; i < expected.length; i++) {
+            Assertions.assertTrue(expected[i].equals(actual[i]));
+        }
+    }
+
 }
