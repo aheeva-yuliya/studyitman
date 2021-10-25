b40cc0fa120eb9f90a8bfdbac09ccb79d19f6cdd
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-10-25 10:27:08 +0300

    Task551 doublyLinkedList(initial)

--- src/main/java/tasks/Task551.java
+++ src/main/java/tasks/Task551.java
@@ -8,16 +8,19 @@ import entities.Node;
 public class Task551 {
 
     /**
-     * Creates three Node objects that link to each other down the chain.
+     * Creates three Node objects that link to each other up and down the chain.
      *
      * @cpu O(1)
      * @ram O(1)
      *
-     * @return a list with the elements 12, 99, 37.
+     * @return a  doubly linked list with the elements 12, 99, 37.
      */
     public static Node buildLinkedList() {
         Node last = new Node(37, null);
         Node second = new Node(99, last);
-        return new Node(12, second);
+        last.setPrev(second);
+        Node first = new Node(12, second, null);
+        second.setPrev(first);
+        return first;
     }
 }
