5138caa700bf147f2f0701954beecb606682494d
aheeva-yuliya <yuliya-aheeva@mail.ru>
2021-10-28 15:10:50 +0300

    refactor Node (initial)

--- src/test/java/tasks/Task551Tests.java
+++ src/test/java/tasks/Task551Tests.java
@@ -1,6 +1,5 @@
 package tasks;
 
-import entities.Node;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,7 @@ public class Task551Tests {
     public class Build {
         @Test
         public void shouldBuildLinkedListWhenSingle() {
-            Node node = Task551.buildLinkedList();
+            Task551.Node node = Task551.buildLinkedList();
             Assertions.assertEquals(12, node.getElement());
             Assertions.assertEquals(99, node.getNext().getElement());
             Assertions.assertEquals(37, node.getNext().getNext().getElement());
@@ -18,9 +17,75 @@ public class Task551Tests {
 
         @Test
         public void shouldBuildLinkedListWhenDouble() {
-            Node node = Task551.buildLinkedList();
+            Task551.Node node = Task551.buildLinkedList();
             Assertions.assertEquals(12, node.getNext().getPrev().getElement());
             Assertions.assertEquals(99, node.getNext().getNext().getPrev().getNext().getPrev().getElement());
+        }
+    }
+
+    @Nested
+    public class NodeTests {
+        @Nested
+        public class Get {
+            @Test
+            public void shouldGetElementsWhenTryGetFromDifferentLinks() {
+                Task551.Node last = new Task551.Node(37, null, null);
+                Task551.Node second = new Task551.Node(99, last, null);
+                last.setPrev(second);
+                second.setNext(last);
+                Task551.Node first = new Task551.Node(12, second, null);
+                second.setPrev(first);
+                Assertions.assertEquals(12, first.getElement());
+                Assertions.assertEquals(99, second.getElement());
+                Assertions.assertEquals(37, last.getElement());
+            }
+
+            @Test
+            public void shouldGetNextLinkWhenHas() {
+                Task551.Node last = new Task551.Node(37, null, null);
+                Task551.Node second = new Task551.Node(99, last, null);
+                Task551.Node first = new Task551.Node(12, second, null);
+                second.setPrev(first);
+                last.setPrev(second);
+                Assertions.assertSame(second, first.getNext());
+                Assertions.assertSame(last, second.getNext());
+                Assertions.assertNull(last.getNext());
+            }
+
+            @Test
+            public void shouldGetPreviousLinkWhenHas() {
+                Task551.Node last = new Task551.Node(37, null, null);
+                Task551.Node second = new Task551.Node(99, last, null);
+                last.setPrev(second);
+                Task551.Node first = new Task551.Node(12, second, null);
+                second.setPrev(first);
+                Assertions.assertSame(first, second.getPrev());
+                Assertions.assertSame(second, last.getPrev());
+                Assertions.assertNull(first.getPrev());
+            }
+        }
+
+        @Nested
+        public class Set {
+            @Test
+            public void shouldSetNextLinkWhenHas() {
+                Task551.Node last = new Task551.Node(37, null, null);
+                Task551.Node second = new Task551.Node(99, last, null);
+                Task551.Node first = new Task551.Node(12, second, null);
+                second.setPrev(first);
+                last.setPrev(second);
+                Assertions.assertSame(second, first.getNext());
+                Assertions.assertSame(last, second.getNext());
+                Assertions.assertNull(last.getNext());
+            }
+
+            @Test
+            public void shouldSetPreviousLinkWhenHas() {
+                Task551.Node last = new Task551.Node(37, null, null);
+                Task551.Node first = new Task551.Node(12, last, null);
+                last.setPrev(first);
+                Assertions.assertSame(first, last.getPrev());
+            }
         }
     }
 }
