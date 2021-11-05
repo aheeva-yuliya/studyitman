fd865ff76bce0bcb802c7f750927a1805a1876e8
yuliya <yuliya.aheeva@mail.ru>
2021-11-05 18:10:28 +0300

    QueueSpringApplication and Controller (initial)

---
+++ src/main/java/services/queue/QueueController.java
@@ -0,0 +1,67 @@
+package services.queue;
+
+import collections.ArrayList;
+import entities.Ticket;
+import org.springframework.web.bind.annotation.*;
+import tasks.QueueManagementSystem;
+
+/**
+ * QueueController
+ */
+@RestController
+public class QueueController {
+    private QueueManagementSystem queue = new QueueManagementSystem("place");
+
+    /**
+     * Creates the next ticket.
+     *
+     * @cpu O(1)
+     * @ram O(1)
+     *
+     * @return String information of the next ticket.
+     */
+    @GetMapping("/api/queue/nextTicket")
+    public String getNextTicket() {
+        Ticket ticket = queue.getNextTicket();
+        return ticket.toString();
+    }
+
+    /**
+     * Returns the total number of issued tickets.
+     *
+     * @cpu O(1)
+     * @ram O(1)
+     *
+     * @return String information about the number of issued tickets for the current queue.
+     */
+    @GetMapping("/api/queue/totalTickets")
+    public String getTotalTickets() {
+        int total = queue.getTotalTickets();
+        return "" + total + "";
+    }
+
+    /**
+     * Starts a new day.
+     *
+     * @cpu O(1)
+     * @ram O(1)
+     */
+    @PostMapping("/api/queue/toNextWorkDay")
+    public void postNextWorkDay() {
+        queue.toNextWorkDay();
+    }
+
+    /**
+     * Creates an attendance list by day.
+     *
+     * @cpu O(1)
+     * @ram O(n), n = the length of instance variable visits
+     *
+     * @return a new object of the ArrayList class.
+     */
+    @GetMapping("/api/queue/getVisitsByDays")
+    public String getVisitsByDay() {
+        ArrayList arrayList = queue.getVisitsByDay();
+        return arrayList.toString();
+    }
+}
---
+++ src/test/java/services/queue/QueueControllerTests.java
@@ -0,0 +1,43 @@
+package services.queue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class QueueControllerTests {
+
+    @Nested
+    public class EndToEnd {
+        @Test
+        public void shouldReturnTwoTicketsWhenTwoDaysWithOneTicket () {
+            QueueController controller = new QueueController();
+            String expected = "Ticket{number=1, place='place'}";
+            String actual = controller.getNextTicket();
+            Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals("1", controller.getTotalTickets());
+            Assertions.assertEquals("[1]", controller.getVisitsByDay());
+            controller.postNextWorkDay();
+            String expectedSecond = "Ticket{number=1, place='place'}";
+            String actualSecond = controller.getNextTicket();
+            Assertions.assertEquals(expectedSecond, actualSecond);
+            Assertions.assertEquals("2", controller.getTotalTickets());
+            Assertions.assertEquals("[1, 1]", controller.getVisitsByDay());
+        }
+
+        @Test
+        public void shouldReturnOneTicketWhenTwoDaysWithoutTicketsLastDayOneTicket () {
+            QueueController controller = new QueueController();
+            Assertions.assertEquals("0", controller.getTotalTickets());
+            Assertions.assertEquals("[0]", controller.getVisitsByDay());
+            controller.postNextWorkDay();
+            Assertions.assertEquals("0", controller.getTotalTickets());
+            Assertions.assertEquals("[0, 0]", controller.getVisitsByDay());
+            controller.postNextWorkDay();
+            String expected = "Ticket{number=1, place='place'}";
+            String actual  = controller.getNextTicket();
+            Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals("1", controller.getTotalTickets());
+            Assertions.assertEquals("[0, 0, 1]", controller.getVisitsByDay());
+        }
+    }
+}
