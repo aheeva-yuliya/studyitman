445ab5e982a9232d5531cea79487096a743617a8
yuliya <yuliya.aheeva@mail.ru>
2021-11-08 09:30:27 +0300

    pull request #5 (initial)

--- src/main/java/services/example/EventExampleController.java
+++ src/main/java/services/example/EventExampleController.java
@@ -3,16 +3,34 @@ package services.example;
 import entities.Event;
 import org.springframework.web.bind.annotation.*;
 
-
+/**
+ * EventExampleController
+ */
 @RestController
 public class EventExampleController {
-    Event event;
+    private Event event;
 
+    /**
+     * Gets JSON Event and saves it.
+     *
+     * @cpu O(1)
+     * @ram O(1)
+     *
+     * @param body JSON Event
+     */
     @PostMapping("/api/event")
     public void saveEvent(@RequestBody Event body) {
         event = body;
     }
 
+    /**
+     * Returns the last remembered Event in JSON format.
+     *
+     * @cpu O(1)
+     * @ram O(1)
+     *
+     * @return the last remembered Event.
+     */
     @GetMapping("/api/event")
     public Event getEvent() {
         return event;
