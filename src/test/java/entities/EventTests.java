65b5a3197e9f6da36239b3d69a67952641366ce7
yuliya <yuliya.aheeva@mail.ru>
2021-11-24 09:06:39 +0300

    event tests

--- src/test/java/entities/EventTests.java
+++ src/test/java/entities/EventTests.java
@@ -9,8 +9,8 @@ public class EventTests {
     public class ToString {
         @Test
         public void shouldReturnStringWhenEventNotNull() {
-            Event event = new Event(1918, 3, 25, "The independence of the BNR");
-            String expected = "Event{year=1918, month=3, day=25, name='The independence of the BNR'}";
+            Event event = new Event(1918, 3, 25, "The independence of the BNR", 1);
+            String expected = "Event{year=1918, month=3, day=25, name='The independence of the BNR', id=1}";
             String actual = event.toString();
             Assertions.assertEquals(expected, actual);
         }
@@ -20,35 +20,35 @@ public class EventTests {
     public class Equals {
         @Test
         public void shouldReturnFalseWhenDifferentEvents() {
-            Event a = new Event(1991, 9, 8, "my birthday");
-            Event b = new Event(1992, 2, 20, "igor's birthday");
+            Event a = new Event(1991, 9, 8, "my birthday", 1);
+            Event b = new Event(1992, 2, 20, "igor's birthday", 2);
             Assertions.assertFalse(a.equals(b));
         }
 
         @Test
         public void shouldReturnTrueWhenSameEvents() {
-            Event a = new Event(1991, 9, 8, "my birthday");
-            Event b = new Event(1991, 9, 8, "my birthday");
+            Event a = new Event(1991, 9, 8, "my birthday", 1);
+            Event b = new Event(1991, 9, 8, "my birthday", 1);
             Assertions.assertTrue(a.equals(b));
         }
 
         @Test
         public void shouldReturnFalseWhenThatNull() {
-            Event a = new Event(2022, 2, 28, "A");
+            Event a = new Event(2022, 2, 28, "A", 1);
             Assertions.assertFalse(a.equals(null));
         }
 
         @Test
         public void shouldReturnTrueWhenSameEventsContainNull() {
-            Event a = new Event(2024, 2, 28, null);
-            Event b = new Event(2024, 2, 28, null);
+            Event a = new Event(2024, 2, 28, null, 1);
+            Event b = new Event(2024, 2, 28, null, 1);
             Assertions.assertTrue(a.equals(b));
         }
 
         @Test
         public void shouldReturnFalseWhenOnlyOneEventContainsNull() {
-            Event a = new Event(2024, 2, 28, null);
-            Event b = new Event(2024, 2, 28, "interesting");
+            Event a = new Event(2024, 2, 28, null, 1);
+            Event b = new Event(2024, 2, 28, "interesting", 1);
             Assertions.assertFalse(a.equals(b));
         }
     }
@@ -57,22 +57,22 @@ public class EventTests {
     public class Compare {
         @Test
         public void shouldReturnNegativeWhenFirstHappenedBefore() {
-            Event a = new Event(1991, 9, 8, "a");
-            Event b = new Event(1992, 2, 20, "b");
+            Event a = new Event(1991, 9, 8, "a", 1);
+            Event b = new Event(1992, 2, 20, "b", 2);
             Assertions.assertEquals(-1, a.compareTo(b));
         }
 
         @Test
         public void shouldReturnZeroWhenHappenedSameDate() {
-            Event a = new Event(1991, 9, 8, "a");
-            Event b = new Event(1991, 9, 8, "b");
+            Event a = new Event(1991, 9, 8, "a", 1);
+            Event b = new Event(1991, 9, 8, "b", 2);
             Assertions.assertEquals(0, a.compareTo(b));
         }
 
         @Test
         public void shouldReturnPositiveWhenFirstHappenedAfter() {
-            Event a = new Event(1991, 9, 8, "a");
-            Event b = new Event(1992, 2, 20, "b");
+            Event a = new Event(1991, 9, 8, "a", 1);
+            Event b = new Event(1992, 2, 20, "b", 2);
             Assertions.assertEquals(1, b.compareTo(a));
         }
     }
